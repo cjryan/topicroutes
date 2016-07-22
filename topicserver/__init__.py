0093e520efd5ff7589ed485dbd0db9dd22dfae0e
Jeremy Wright <jeremy.wright@commscope.com>
2016-07-21 22:54:37 -0700

    Adding security templates.

--- topicserver/__init__.py
+++ topicserver/__init__.py
@@ -4,9 +4,23 @@ The flask application package.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_object('topicserver.config')
 db = SQLAlchemy(app)
 
+admin = Admin(app, name='topicserver', template_mode='bootstrap3')
+
 from topicserver import views, models
+from flask_admin.contrib.sqla import ModelView
+
+admin.add_view(ModelView(models.Person, db.session))
+
+#@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+        get_url=url_for)
