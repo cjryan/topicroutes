c74ff8a089769eb53fa92c81038dee978360bfd8
Jeremy Wright <jeremy.wright@commscope.com>
2016-07-21 23:14:53 -0700

    working through the security stuff.

--- topicserver/__init__.py
+++ topicserver/__init__.py
@@ -5,6 +5,9 @@ The flask application package.
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required, current_user
+from flask_security.utils import encrypt_password
+
 
 app = Flask(__name__)
 app.config.from_object('topicserver.config')
@@ -16,6 +19,36 @@ from topicserver import views, models
 from flask_admin.contrib.sqla import ModelView
 
 admin.add_view(ModelView(models.Person, db.session))
+
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return self.email
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
 
 #@security.context_processor
 def security_context_processor():
